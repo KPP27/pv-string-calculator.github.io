f6050fdd6a38348d6976aa40dc54d8a70819ae36
KPP27 <101493107+KPP27@users.noreply.github.com>
2025-12-05 11:12:39 +0200

    Delete tests/test_calc.py

--- tests/test_calc.py
+++
@@ -1,8 +0,0 @@
-from src.calc import voc_cold, compute_max_panels
-
-def test_voc_cold():
-    assert round(voc_cold(40, -0.3, -10), 2) == 41.32
-
-def test_compute_max_panels():
-    result = compute_max_panels(40, 33, -0.3, -10, 200, 500, 600)
-    assert result['N_max'] <= result['N_voc_max']
